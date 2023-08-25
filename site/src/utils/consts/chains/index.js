bf080ca4b21aae018702c3b29d0974dcf2f093ac
DFinn-blockchain <Bharathcoorg7@gmail.com>
2023-08-26 00:02:34 +0530

    Initial commit

---
+++ site/src/utils/consts/chains/index.js
@@ -0,0 +1,23 @@
+import litentry from "./litentry";
+import statemine from "./statemine";
+import statemint from "./statemint";
+import westmint from "./westmint";
+import litmus from "./litmus";
+import westendCollectives from "./westendCollectives";
+import collectives from "./collectives";
+import kusama from "./kusama";
+import polkadot from "./polkadot";
+
+const chains = {
+  kusama,
+  polkadot,
+  litentry,
+  litmus,
+  statemine,
+  statemint,
+  westmint,
+  "westend-collectives": westendCollectives,
+  collectives,
+};
+
+export default chains;
